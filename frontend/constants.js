3f96171f6261ac603fa67156d4242eeeb77fa2f6
Akash Singh <laughing.buddha.lab@gmail.com>
2025-08-26 06:40:51 +0200

    tweak info text

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,5 +1,5 @@
 // Constants for Bhai Jaan Academy frontend
-export const ABOUT_TEXT = `Thanks to the internet, we can learn about almost any topic. However, it can be hard to know where to start. To solve this problem, I created <strong>Bhai Jaan Academy</strong>. You simply give your <strong>email address</strong> and the <strong>topic</strong> you wish to learn. Bhai Jaan Academy uses AI to generate a comprehensive learning plan and send you regular content to read. If you find this concept useful, please purchase your first topic. With this purchase, you get access to:<br><br>
+export const ABOUT_TEXT = `Thanks to the internet, we can learn almost anything. But, where do you start? Therefore, I created <strong>Bhai Jaan Academy</strong>. You give your <strong>email address</strong> and the <strong>topic</strong> you wish to learn. You will receive a comprehensive learning plan and regular content to read. If you find this useful, please purchase your first topic. With this purchase, you get access to:<br><br>
 1. All of the generated content for your topic<br>
 2. Access to members-only channel to provide feedback and vote for new features for a month<br>
 3. Regenerate your content with the latest features<br><br>

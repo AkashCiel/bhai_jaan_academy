df1b6edb2d27ddff6f10bccb1a9facdd6140aeee
Akash Singh <laughing.buddha.lab@gmail.com>
2025-09-02 18:49:51 +0200

    update help text

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,7 +1,7 @@
 // Constants for Bhai Jaan Academy frontend
-export const ABOUT_TEXT = `Thanks to the internet, we can learn almost anything. But, where do you start? Therefore, I created <strong>Bhai Jaan Academy</strong>. You give your <strong>email address</strong> and the <strong>topic</strong> you wish to learn. You will receive a comprehensive learning plan and regular content to read. If you find this useful, please purchase your first topic. With this purchase, you get access to:<br><br>
+export const ABOUT_TEXT = `Thanks to the internet, we can learn almost anything. But, where do you start? Therefore, I created <strong>Bhai Jaan Academy</strong>. You give your <strong>email address</strong> and the <strong>topic</strong> you wish to learn. You will receive a comprehensive learning plan and regular content to read. If you find this useful, please register your first topic. With this, you get access to:<br><br>
 1. All of the generated content for your topic<br>
-2. Access to members-only channel to provide feedback and vote for new features for a month<br>
+2. Provide feedback and vote for new features<br>
 3. Regenerate your content with the latest features<br><br>
 Watch the demo video and let's go!`;
 
